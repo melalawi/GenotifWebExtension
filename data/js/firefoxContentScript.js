30dce938853d931ef43b3956cc07c7f2a1f1330f
insentience <elalawi_mohamed@yahoo.ca>
2015-09-06 14:48:45 -0500

    firefox jpm support

--- data/js/firefoxContentScript.js
+++ data/js/firefoxContentScript.js
@@ -2,14 +2,25 @@
 
 "use strict";
 
-var contentScript = new GTE_SCRIPT.ContentScript();
+var messageListeners = {},
+    contentScript,
+    adaptor;
+
+adaptor = {
+    localDirectory: function(path) {
+        return self.options.parentDirectory + path;
+    },
+
+    sendMessage: function(message, data, onResponse) {
+        self.port.once(message, onResponse);
+        self.port.emit('contentScriptMessage', {message: message, data: data});
+    }
+};
 
 function initialize(settings) {
+    contentScript = new GTE_SCRIPT.ContentScript(adaptor);
     contentScript.initialize(settings);
 }
 
-self.port.once("data", function(data){
-    initialize(data);
-});
-
+self.port.once('geneData', initialize);
 })();
